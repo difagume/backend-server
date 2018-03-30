b8b31b5e734dfcd9f02f43bb637e829bb6b32ca1
Diego Fabricio <difagume@gmail.com>
2018-03-29 23:41:23 -0500

    Validar token de google en el backend

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '458229930749-sr6sma82ppdcr8546mn3qtilsuqbb7a7.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'hBIpTc2nEUj21N8QYgPzmbLP';
