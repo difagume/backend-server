c003077a46523ad804c8d43b779817573b37cf2f
Diego Fabricio <difagume@gmail.com>
2018-04-07 23:37:20 -0500

    Habilitando CORS

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const PUERTO = require('./config/config').PUERTO;
 var app = express();
 
 // Control de acceso HTTP (CORS)
-/* app.use(function (req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     next();
-}); */
+});
 
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
